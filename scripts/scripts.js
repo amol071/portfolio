4e374c6007887e579651cda9ca781b23e1105a6e
Amol Vyavaharkar <amolvyavaharkar@gmail.com>
2024-11-24 19:55:28 +0530

    revamped portfolio
    made a simple quick portfolio page.
    contains home page, about me page, contact me page.
    separate styles and js script page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,49 +1,8 @@
-$(document).ready(function() {
-    // Smooth scrolling for navigation links
-    $('a.nav-link').on('click', function(e) {
-        if (this.hash !== "") {
-            e.preventDefault();
-            const hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-                window.location.hash = hash;
-            });
-        }
-    });
-
-    $('#carouselExampleIndicators').carousel({
-        interval: 3000
-    });
-
-    $('#contact-form').on('submit', function(e) {
-        e.preventDefault();
-        alert('Thank you for contacting me!');
-        $('#contact-form')[0].reset();
-    });
-
-    // Navbar scroll effect
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 50) {
-            $('#navbar').removeClass('initial').addClass('scrolled');
-            $('.navbar-brand').removeClass('initial').addClass('scrolled');
-        } else {
-            $('#navbar').removeClass('scrolled').addClass('initial');
-            $('.navbar-brand').removeClass('scrolled').addClass('initial');
-        }
-
-        // Show back to top button
-        if ($(document).scrollTop() > $('#about').offset().top - 100) {
-            $('#back-to-top').fadeIn();
-        } else {
-            $('#back-to-top').fadeOut();
-        }
-    });
-
-    // Back to top button click event
-    $('#back-to-top').on('click', function(e) {
-        e.preventDefault();
-        $('html, body').animate({ scrollTop: 0 }, 800);
-    });
-});
+window.onload = () => {
+    const loadingScreen = document.getElementById('loading-screen');
+    if (loadingScreen) {
+        setTimeout(() => {
+            loadingScreen.classList.add('hidden'); // Add a class for fade-out
+        }, 1500);
+    }
+};
